4d1dd2e9720bba392e5ffe97d0d55ebe3f75841a
Shyam Shankar H R <shyam.rani@campus.lmu.de>
2023-02-02 02:34:13 +0100

    Added candidates and basic UI

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,12 +2,27 @@ pragma solidity ^0.5.16;
 
 contract Election {
 
-	// Store candidate
-	// Read candidate
-	string public candidate;
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store Candidates
+	// Fetch Candidates
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates count
+	uint public candidatesCount;
+
 	// Constructor
 	constructor() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
 }
