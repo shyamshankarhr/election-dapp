4f995218a819f710de36c65867c8de7a0c83b67b
Shyam Shankar H R <shyam.rani@campus.lmu.de>
2023-02-02 05:20:14 +0100

    Added Voting feature

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.5.16;
 
 contract Election {
 
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+
 	// Model a candidate
 	struct Candidate {
 		uint id;
@@ -14,6 +17,9 @@ contract Election {
 	// Store Candidates count
 	uint public candidatesCount;
 
+	// voted event
+	event votedEvent(uint indexed _candidateId);
+
 	// Constructor
 	constructor() public {
 		addCandidate("Candidate 1");
@@ -25,4 +31,21 @@ contract Election {
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
+	function vote(uint _candidateId) public {
+
+		// require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		// record that voter has voted
+		voters[msg.sender] = true;
+
+		// update candidate vote count
+		candidates[_candidateId].voteCount ++;
+
+		// trigger voting event
+		emit votedEvent(_candidateId);
+	}
 }
