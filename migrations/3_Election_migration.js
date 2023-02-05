8f5202fe18044ac6a674059186d2137072c54d79
cromicron <darionalis.gmail.com>
2023-02-05 19:43:27 +0100

    eligibility of voters added to election.sol, updated 3_Election_migration to take registry

--- migrations/3_Election_migration.js
+++ migrations/3_Election_migration.js
@@ -1,5 +1,22 @@
-var Election = artifacts.require("./Election.sol");
+const fs = require('fs');
+const registerContract = JSON.parse(fs.readFileSync('../build/contracts/Register.json', 'utf8'));
+const Election = artifacts.require("./Election.sol");
+const theQuestion = "what is superior?"
+const candidateNames = ["Chocolate", "Vanilla"];
+//const registrationContractAddress = registerContract.networks[5].address;
+console.log(registerContract.networks);
+const registrationContractAddress = registerContract.networks[5777].address;
+const startElectionTime = 1675431565;
+const endElectionTime = 1775431565;
 
 module.exports = function(deployer) {
-  deployer.deploy(Election);
+  deployer.deploy(
+    Election,
+    theQuestion,
+    candidateNames,
+    registrationContractAddress,
+    startElectionTime,
+    endElectionTime
+    );
 };
+
